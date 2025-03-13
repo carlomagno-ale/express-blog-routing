05ed69025ddf4b32bbeaa235bfccde0fbd946dda
carlomagno-ale <carlomagno.ale@gmail.com>
2025-03-13 15:56:15 +0100

    add routers with CRUD operation

---
+++ routers/posts.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const router = express.Router();
+
+// index
+router.get('/', function(req, res) {
+res.send('visualizza tutti i post');
+});
+
+//show
+router.get('/:id', () => {
+    res.send(`visualizza solo i post con id:${req.params.id}`)
+});
+
+//create/store
+router.post('/', (req, res) => {
+    res.send('crea un nuovo post')
+});
+
+//update
+router.put('/:id', (req, res) => {
+    res.send(`modifica totalemnte un post con id:${req.params.id}`)
+});
+
+//update/modify 
+router.patch('/:id', (req, res) => {
+    res.send(`modifica parzialmente un post con id ${req.params.id}`)
+});
+
+//delete
+router.delete('/:id', (req, res) => {
+    res.send(`elimina un post con id ${req.params.id}`)
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express();
 const port = 3000;
+const postsRouter = require('./routers/posts')
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
 });
 
+app.get('/', (req, res) => {
+    res.send('welcome')
+})
+
+app.use('./api/v1/posts', postsRouter)
